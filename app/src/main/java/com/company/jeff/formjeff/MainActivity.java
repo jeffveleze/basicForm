048b08552aee0daad0b91aa0a990f65b6f6b85bf
jeffveleze <jeffrsk8@hotmail.com>
2016-07-25 16:13:43 -0500

    restaurant commit

--- app/src/main/java/com/company/jeff/formjeff/MainActivity.java
+++ app/src/main/java/com/company/jeff/formjeff/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //Log.d("IMPRIMIR", "This is other message");
 
     }
 
